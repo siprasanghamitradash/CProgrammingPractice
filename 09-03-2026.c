aa9ac9c1da78b917f52b0dd7b567c56bc3e99c1f
siprasanghamitradash <dash.sipra.sanghamitra@gmail.com>
2026-03-09 10:24:50 +0530

    March 9

--- 09-03-2026.c
+++ 09-03-2026.c
@@ -1,12 +1,14 @@
 // Passing argumets by reference
 #include <stdio.h>
-int num(int *a){
+int num(int *a)
+{
     *a = 30;
 }
-int main() {
+int main()
+{
     int a = 10;
     num(&a);
-    printf("%d",a);
+    printf("%d", a);
     return 0;
 }
 
@@ -14,47 +16,61 @@ int main() {
 #include <stdio.h>
 #include <math.h>
 
-int main() {
+int main()
+{
     int a = 25;
-    printf("%.f\n",sqrt(a));
-    printf("%.f\n",ceil(4.2));
-    printf("%.f\n",floor(4.7));
-    printf("%.f\n",round(4.7));
-    printf("%.f\n",pow(2,3));
+    printf("%.f\n", sqrt(a));
+    printf("%.f\n", ceil(4.2));
+    printf("%.f\n", floor(4.7));
+    printf("%.f\n", round(4.7));
+    printf("%.f\n", pow(2, 3));
     return 0;
 }
 
 // Auto datatype
 #include <stdio.h>
 // stores in RAM
-int main() {
+int main()
+{
     auto int a = 25;
-    printf("%d\n",a);
+    printf("%d\n", a);
     return 0;
 }
 
 // Register datatype
 #include <stdio.h>
 // stores in registers in CPU
-int main() {
+int main()
+{
     register int a = 25;
-    printf("%d\n",a);
+    printf("%d\n", a);
     return 0;
 }
 
 // Static datatype
 #include <stdio.h>
 // makes variable static
-void counter(){
+void counter()
+{
     static int a = 1;
     a++;
-    printf("%d\n",a);
+    printf("%d\n", a);
 }
-int main() {
+int main()
+{
     counter();
     counter();
     counter();
     return 0;
 }
 
-a
+// extern datatype
+#include <stdio.h>
+// gathers global variable
+int a = 10;
+int main()
+{
+    extern int a;
+    printf("%d", a);
+    return 0;
+}
