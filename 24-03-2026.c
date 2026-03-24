daba74baa161fd2fb70cfc635379a20606d2426a
siprasanghamitradash <dash.sipra.sanghamitra@gmail.com>
2026-03-24 09:46:50 +0530

    March 24

---
+++ 24-03-2026.c
@@ -0,0 +1,5 @@
+// 1. count list of names which start n end with vowel
+// 2. find out the largest str in the array
+// 3. find out the smallest str in the array
+// 4. print list of ele frm the array which have more than 3 vowels
+// 5. print list of str which length odd 
