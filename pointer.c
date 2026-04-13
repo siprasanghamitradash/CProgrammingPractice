fbfbaa9c01707af8401462264195ab2f1a9d0e0b
siprasanghamitradash <dash.sipra.sanghamitra@gmail.com>
2026-04-13 10:12:31 +0530

    13 April

--- pointer.c
+++ pointer.c
@@ -1,12 +1,19 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int a = 10;
-    int *p =&a;
-    printf("%d  ",a);
-    printf("%d  ",*p);
-    printf("%d  ",&a);
-    printf("%d  ",p);
+    int *p = &a;
+    printf("%d  ", a);
+    printf("%d  ", *p);
+    printf("%d  ", &a);
+    printf("%d  ", p);
+    int b = 11;
+    int *s = &b;
+    int temp = *s;
+    *s = *p;
+    *p = temp;
+    printf("%d %d", a, b);
     return 0;
 }
